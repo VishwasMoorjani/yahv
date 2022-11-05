fb3bcb83d82b75368a5fd927dc1489b4ec9676d2
Vishwas Moorjani <vishwasmoorjani@gmail.com>
2022-11-05 14:33:43 +0530

    yahv

---
+++ footer.php
@@ -0,0 +1,231 @@
+<footer id="footer">      
+		<div id="footer-content">
+			<h6 class="general-title contact-footer-title">Newsletter</h6>  
+			<div id="widget-newsletter">
+				<div class="container">            
+				  <div class="newsletter col-md-24">
+					<form action="#" method="post" id="mc-embedded-subscribe-form" name="mc-embedded-subscribe-form" target="_blank">
+					  <span class="news-desc">We promise only send the good things</span>
+					  <div class="group_input">
+						<input class="form-control" type="email" placeholder="Your Email Address" name="Email" id="email-input">
+						<div class="unpadding-top"><button class="btn btn-1" type="submit"><i class="fa fa-paper-plane"></i></button></div>
+					  </div>              
+					</form>
+				  </div>						
+				</div>
+			</div>
+			
+			<div class="footer-content footer-content-top clearfix">
+				<div class="container">
+					<div class="footer-link-list col-md-6">
+					  <div class="group">
+						<h5 class="general-title">Company</h5>						
+						<ul class="list-unstyled list-styled">						  
+						  <li class="list-unstyled">
+							<a href="index.php">Yahvi Jewellery</a>
+						  </li>						  
+						  <li class="list-unstyled">
+							<p><i class="fa fa-map-marker" style="padding-right:10px; font-size:20px"></i>134, Frontier Colony, Adarsh Nagar, Jaipur 302004, India</p>
+						  </li>						  
+						  <li class="list-unstyled">
+						  <ul class="list-inline">            
+					<li><a target="_blank" href="#" class="btooltip swing" data-toggle="tooltip" data-placement="bottom" title="" data-original-title="Facebook"><i class="fa fa-facebook" style="font-size:21px"></i></a></li>
+					<li><a target="_blank" href="#" class="btooltip swing" data-toggle="tooltip" data-placement="bottom" title="" data-original-title="Instagram"><i class="fa fa-instagram" style="font-size:21px"></i></a></li>                        
+					         
+				  </ul>
+						  </li>						  
+						 					  
+						</ul>
+					  </div>
+					</div>   
+					<div class="footer-link-list col-md-6">
+					  <div class="group">
+						<h5 class="general-title">Explore </h5>						
+						<ul class="list-unstyled list-styled">						  
+						  <li class="list-unstyled">
+							<a href="index.php">Store</a>
+						  </li>						  
+						  <li class="list-unstyled">
+							<a href="customize.php">Customize</a>
+						  </li>						  
+						  <li class="list-unstyled">
+							<a href="account.html">Size guides</a>
+						  </li>						  
+						  <li class="list-unstyled">
+							<a href="about-us.php">About Us</a>
+						  </li>		
+						  <li class="list-unstyled">
+							<a href="gallery.php">Gallery</a>
+						  </li>					  
+						</ul>
+					  </div>
+					</div>
+					<div class="footer-link-list col-md-6">
+					  <div class="group">
+						<h5 class="general-title">Help</h5>						
+						<ul class="list-unstyled list-styled">						  
+						  <li class="list-unstyled">
+							<a href="contact.php">Contact Us </a>
+						  </li>						  
+						  <li class="list-unstyled">
+							<a href="faqs.php">FAQs</a>
+						  </li>						  
+						  <li class="list-unstyled">
+							<a href="shipping-policy.php">Shipping Policy</a>
+						  </li>						  
+						  <li class="list-unstyled">
+							<a href="Terms-&-Conditions-for-Return-&-Exchange.php">Cancellation, Return & Exchange Policy</a>
+						  </li>						  
+						</ul>
+					  </div>
+					</div>
+					<div class="footer-link-list col-md-6">
+					  <div class="group">
+						<h5 class="general-title">LET'S KEEP IN TOUCH!</h5>						
+						<div class="newsletter ">
+							<form action="#" method="post" id="mc-embedded-subscribe-form" name="mc-embedded-subscribe-form" target="_blank">
+							  
+							  <div class="group_input">
+								<input class="form-control" type="email" placeholder="Your Email Address" name="Email" id="email-input">
+								<div class="unpadding-top"><button class="btn btn-1" type="submit">Subscribe</button></div>
+							  </div>              
+							</form>
+						  </div>
+					  </div>
+					</div>   
+				</div>
+			</div>
+			<div class="footer-content footer-content-bottom clearfix">
+				<div class="container">
+					<div class="copyright col-md-12">Design & Developed By
+						<a href="https://www.gdigitalindia.com/">GdigitalIndia</a>
+					</div>
+					<div id="widget-payment" class="col-md-12">
+						<ul id="payments" class="list-inline animated">
+							<li class="btooltip tada" data-toggle="tooltip" data-placement="top" title="" data-original-title="Visa"><a href="#" class="icons visa"></a></li>
+							<li class="btooltip tada" data-toggle="tooltip" data-placement="top" title="" data-original-title="Mastercard"><a href="#" class="icons mastercard"></a></li>
+							<li class="btooltip tada" data-toggle="tooltip" data-placement="top" title="" data-original-title="American Express"><a href="#" class="icons amex"></a></li>
+							<li class="btooltip tada" data-toggle="tooltip" data-placement="top" title="" data-original-title="Paypal"><a href="#" class="icons paypal"></a></li>
+							<li class="btooltip tada" data-toggle="tooltip" data-placement="top" title="" data-original-title="Moneybookers"><a href="#;" class="icons moneybookers"></a></li>
+						</ul>
+					</div>
+				</div>
+			</div>
+		</div>   
+	</footer>
+	
+	<div id="quick-shop-modal" class="modal in" role="dialog" aria-hidden="false" tabindex="-1" data-width="800">
+		<div class="modal-backdrop in" style="height: 742px;">
+		</div>
+		<div class="modal-dialog rotateInDownLeft animated">
+			<div class="modal-content">
+				<div class="modal-header">
+					<i class="close fa fa-times btooltip" data-toggle="tooltip" data-placement="top" title="" data-dismiss="modal" aria-hidden="true" data-original-title="Close"></i>
+				</div>
+				<div class="modal-body">
+					<div class="quick-shop-modal-bg" style="display: none;">
+					</div>
+					<div class="row">
+						<div class="col-md-12 product-image">
+							<div id="quick-shop-image" class="product-image-wrapper">
+								<a class="main-image"><img class="img-zoom img-responsive image-fly" src="assets/images/1_grande.jpg" data-zoom-image="./assets/images/1.jpg" alt=""/></a>
+								<div id="gallery_main_qs" class="product-image-thumb">
+									<a class="image-thumb active" href="./assets/images/1_grande.jpg" data-image="./assets/images/1_grande.jpg" data-zoom-image="assets/images/1.html"><img src="assets/images/1_compact.jpg" alt=""/></a>
+									<a class="image-thumb" href="./assets/images/2_grande.jpg" data-image="./assets/images/2_grande.jpg" data-zoom-image="assets/images/2.html"><img src="assets/images/2_compact.jpg" alt=""/></a>
+									<a class="image-thumb" href="./assets/images/3_grande.jpg" data-image="./assets/images/3_grande.jpg" data-zoom-image="assets/images/3.html"><img src="assets/images/3_compact.jpg" alt=""/></a>
+									<a class="image-thumb" href="./assets/images/4_grande.jpg" data-image="./assets/images/4_grande.jpg" data-zoom-image="assets/images/4.html"><img src="assets/images/4_compact.jpg" alt=""/></a>
+									<a class="image-thumb" href="./assets/images/5_grande.jpg" data-image="./assets/images/5_grande.jpg" data-zoom-image="assets/images/5.html"><img src="assets/images/5_compact.jpg" alt=""/></a>
+									<a class="image-thumb" href="./assets/images/6_grande.jpg" data-image="./assets/images/6_grande.jpg" data-zoom-image="assets/images/6.html"><img src="assets/images/6_compact.jpg" alt=""/></a>
+								</div>	
+							</div>
+						</div>
+						<div class="col-md-12 product-information">
+							<h1 id="quick-shop-title"><span> <a href="">Ring</a></span></h1>
+							<div id="quick-shop-infomation" class="description">
+								<div id="quick-shop-description" class="text-left">
+									<p>
+										Nam libero tempore, cum soluta nobis est eligendi optio cumque nihil impedit quo minus id quod maxime placeat facere possimus, omnis amet voluptas assumenda est, omnis dolor repellendus quis nostrum.
+									</p>
+									<p>
+										Temporibus autem quibusdam et aut officiis debitis aut rerum dolorem necessitatibus saepe eveniet ut et neque porro quisquam est, qui dolorem ipsum quia dolor s...
+									</p>
+								</div>
+							</div>
+							<div id="quick-shop-container">
+								<!-- <div id="quick-shop-relative" class="relative text-left">
+									<ul class="list-unstyled">
+										<li class="control-group vendor">
+										<span class="control-label">Vendor :</span><a href=""> Vendor 1</a>
+										</li>
+										<li class="control-group type">
+										<span class="control-label">Type :</span><a href=""> Sweaters Wear</a>
+										</li>
+									</ul>
+								</div> -->
+								<form action="#" method="post" class="variants" id="quick-shop-product-actions" enctype="multipart/form-data">
+									<div id="quick-shop-price-container" class="detail-price">
+										<span class="price_sale">&#8360;259.00</span><span class="dash">/</span><del class="price_compare">&#8360;300.00</del>
+									</div>
+									<div class="quantity-wrapper clearfix">
+										<label class="wrapper-title">Quantity</label>
+										<div class="wrapper">
+											<input type="text" id="qs-quantity" size="5" class="item-quantity" name="quantity" value="1">
+											<span class="qty-group">
+											<span class="qty-wrapper">
+											<span class="qty-up" title="Increase" data-src="#qs-quantity">
+											<i class="fa fa-plus"></i>
+											</span>
+											<span class="qty-down" title="Decrease" data-src="#qs-quantity">
+											<i class="fa fa-minus"></i>
+											</span>
+											</span>
+											</span>
+										</div>
+									</div>
+									<div id="quick-shop-variants-container" class="variants-wrapper">
+										<div class="selector-wrapper">
+											<label for="#quick-shop-variants-1293238211-option-0">Color</label>
+											<div class="wrapper">
+												<select class="single-option-selector" data-option="option1" id="#quick-shop-variants-1293238211-option-0" style="z-index: 1000; position: absolute; opacity: 0; font-size: 15px;">
+													<option value="black">black</option>
+													<option value="red">red</option>
+													<option value="blue">blue</option>
+													<option value="purple">purple</option>
+													<option value="green">green</option>
+													<option value="white">white</option>
+												</select>
+												<button type="button" class="custom-style-select-box" style="display: block; overflow: hidden;"><span class="custom-style-select-box-inner" style="width: 264px; display: inline-block;">black</span></button><i class="fa fa-caret-down"></i>
+											</div>
+										</div>
+										<div class="selector-wrapper">
+											<label for="#quick-shop-variants-1293238211-option-1">Size</label>
+											<div class="wrapper">
+												<select class="single-option-selector" data-option="option2" id="#quick-shop-variants-1293238211-option-1" style="z-index: 1000; position: absolute; opacity: 0; font-size: 15px;">
+													<option value="small">small</option>
+													<option value="medium">medium</option>
+													<option value="large">large</option>
+												</select>
+												<button type="button" class="custom-style-select-box" style="display: block; overflow: hidden;"><span class="custom-style-select-box-inner" style="width: 264px; display: inline-block;">small</span></button><i class="fa fa-caret-down"></i>
+											</div>
+										</div>
+									</div>
+									<div class="others-bottom">
+										<input id="quick-shop-add" class="btn small add-to-cart" type="submit" name="add" value="Add to Cart" style="opacity: 1;">
+									</div>
+								</form>
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+
+	<div class="country">
+		<select>
+			
+		<option value="">Choose Currency</option>
+			<option value="">india</option>
+			<option value="">USA</option>
+		</select>
+	</div>
